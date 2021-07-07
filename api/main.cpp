4d8ee8839f90dd54a02cd890780edb94d3099972
Manav Rathod <manav.rathod@berkeley.edu>
2021-07-06 18:07:05 -0700

    added get categories route

--- api/main.cpp
+++ api/main.cpp
@@ -24,18 +24,32 @@ int main(int argc, char *argv[])
     ([]()
      { return "Hello world"; });
 
-    CROW_ROUTE(app, "/add_category").methods("POST"_method)
-    ([&](const crow::request& req)
-     {
+    CROW_ROUTE(app, "/add_category").methods("POST"_method)([&](const crow::request &req) {
         auto x = crow::json::load(req.body);
         if (!x)
             return crow::response(400);
-         category cat(x["username"].s(), x["category_name"].s());
-         transaction t(db->begin());
-         db->persist(cat);
-         t.commit();
-         return crow::response(200);
-     });
+        category cat(x["username"].s(), x["category_name"].s());
+        transaction t(db->begin());
+        db->persist(cat);
+        t.commit();
+        return crow::response(200);
+    });
+
+    CROW_ROUTE(app, "/get_categories").methods("POST"_method)([&](const crow::request &req) {
+        auto body = crow::json::load(req.body);
+        typedef odb::query<category> query;
+        typedef odb::result<category> result;
+        transaction t(db->begin());
+        result r(db->query<category>(query::username == body["username"].s()));
+
+        std::vector<string> categories;
+        for (result::iterator i (r.begin ()); i != r.end (); ++i) {
+            categories.push_back(i->get_category_name());
+        }
+        crow::json::wvalue ret;
+        ret["categories"] = categories;
+        return ret;
+    });
 
     app.port(8080).multithreaded().run();
 }
