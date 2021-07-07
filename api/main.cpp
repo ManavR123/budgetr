0067018c244ba0e7853abe4a4fcb408d6e6be8af
ManavR123 <manav.rathod@berkeley.edu>
2021-07-07 10:48:32 -0700

    got rid of hello world route

--- api/main.cpp
+++ api/main.cpp
@@ -63,9 +63,6 @@ int main(int argc, char *argv[]) {
   unique_ptr<database> db(create_database(argc, argv));
   crow::SimpleApp app;
 
-  CROW_ROUTE(app, "/")
-  ([]() { return "Hello world"; });
-
   CROW_ROUTE(app, "/add_category")
       .methods("POST"_method)([&](const crow::request &req) {
         auto body = crow::json::load(req.body);
