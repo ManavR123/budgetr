7706fdcef64bc557961257ad6b054d45b814d94e
Manav Rathod <manav.rathod@berkeley.edu>
2021-07-06 18:52:02 -0700

    formatted code; don't add category if user already has

--- api/main.cpp
+++ api/main.cpp
@@ -7,49 +7,63 @@
 #include "crow_all.h"
 #include "database.h" // create_database
 
-#include "category/category.h"
 #include "category/category-odb.hxx"
-#include "purchase/purchase.h"
+#include "category/category.h"
 #include "purchase/purchase-odb.hxx"
+#include "purchase/purchase.h"
 
 using namespace std;
 using namespace odb::core;
 
-int main(int argc, char *argv[])
-{
-    unique_ptr<database> db(create_database(argc, argv));
-    crow::SimpleApp app;
+std::vector<string> get_all_categories(unique_ptr<database> &db,
+                                       const std::string &username) {
+  typedef odb::query<category> query;
+  typedef odb::result<category> result;
+  transaction t(db->begin());
+  result r(db->query<category>(query::username == username));
 
-    CROW_ROUTE(app, "/")
-    ([]()
-     { return "Hello world"; });
+  std::vector<string> categories;
+  for (result::iterator i(r.begin()); i != r.end(); ++i) {
+    categories.push_back(i->get_category_name());
+  }
+  return categories;
+}
 
-    CROW_ROUTE(app, "/add_category").methods("POST"_method)([&](const crow::request &req) {
-        auto x = crow::json::load(req.body);
-        if (!x)
-            return crow::response(400);
-        category cat(x["username"].s(), x["category_name"].s());
+int main(int argc, char *argv[]) {
+  unique_ptr<database> db(create_database(argc, argv));
+  crow::SimpleApp app;
+
+  CROW_ROUTE(app, "/")
+  ([]() { return "Hello world"; });
+
+  CROW_ROUTE(app, "/add_category")
+      .methods("POST"_method)([&](const crow::request &req) {
+        auto body = crow::json::load(req.body);
+        if (!body)
+          return crow::response(400, "Body-Is-Empty");
+
+        auto categories = get_all_categories(db, body["username"].s());
+        for (std::vector<string>::iterator cat(categories.begin());
+             cat != categories.end(); ++cat) {
+          if (cat->compare(body["category_name"].s()) == 0) {
+            return crow::response(400, "Category-Already-Exists");
+          }
+        }
+
+        category cat(body["username"].s(), body["category_name"].s());
         transaction t(db->begin());
         db->persist(cat);
         t.commit();
         return crow::response(200);
-    });
+      });
 
-    CROW_ROUTE(app, "/get_categories").methods("POST"_method)([&](const crow::request &req) {
+  CROW_ROUTE(app, "/get_categories")
+      .methods("POST"_method)([&](const crow::request &req) {
         auto body = crow::json::load(req.body);
-        typedef odb::query<category> query;
-        typedef odb::result<category> result;
-        transaction t(db->begin());
-        result r(db->query<category>(query::username == body["username"].s()));
-
-        std::vector<string> categories;
-        for (result::iterator i (r.begin ()); i != r.end (); ++i) {
-            categories.push_back(i->get_category_name());
-        }
         crow::json::wvalue ret;
-        ret["categories"] = categories;
+        ret["categories"] = get_all_categories(db, body["username"].s());
         return ret;
-    });
+      });
 
-    app.port(8080).multithreaded().run();
+  app.port(8080).multithreaded().run();
 }
